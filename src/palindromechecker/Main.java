abb347fccfee731ebccc543d252fff8cfb1a6160
vimal <ervimalraj96@gmail.com>
2026-02-16 18:48:21 +0530

    UC3: Implement palindrome check using for-loop string reversal

--- src/palindromechecker/Main.java
+++ src/palindromechecker/Main.java
@@ -1,10 +1,13 @@
 package palindromechecker;
 import palindromechecker.uc1.WelcomeMessage;
 import palindromechecker.uc2.HardcodedPalindromeCheck;
+import palindromechecker.uc3.ReverseUsingLoopCheck;
+
 
 public class Main {
     public static void main(String[] args) {
         WelcomeMessage.display();
         HardcodedPalindromeCheck.execute();
+        ReverseUsingLoopCheck.execute();
     }
 }
--- src/palindromechecker/uc3/ReverseUsingLoopCheck.java
+++ src/palindromechecker/uc3/ReverseUsingLoopCheck.java
@@ -1,4 +1,20 @@
 package palindromechecker.uc3;
 
 public class ReverseUsingLoopCheck {
+
+    public static void execute() {
+
+        String original = "level";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome (Loop Reverse)");
+        } else {
+            System.out.println(original + " is NOT a Palindrome (Loop Reverse)");
+        }
+    }
 }
