eebea28d968a7aab4676896f8ea265d7d3f8a7e6
vimal <ervimalraj96@gmail.com>
2026-02-16 18:34:32 +0530

    UC2: Implement hardcoded string palindrome validation

--- src/palindromechecker/Main.java
+++ src/palindromechecker/Main.java
@@ -1,8 +1,10 @@
 package palindromechecker;
 import palindromechecker.uc1.WelcomeMessage;
+import palindromechecker.uc2.HardcodedPalindromeCheck;
 
 public class Main {
     public static void main(String[] args) {
         WelcomeMessage.display();
+        HardcodedPalindromeCheck.execute();
     }
 }
